efc3ed356dd112d45acb6ee2457d38b5140bbff5
reedt <dharit.tan@gmail.com>
2020-05-29 17:58:52 -0400

    reeds working

--- sketch.js
+++ sketch.js
@@ -34,6 +34,7 @@ function setup() {
     // sdlogo = loadImage('assets/sd-logo.png');
     ufo = loadImage('assets/ufo.png');
     cat = loadImage('assets/coolcat.png');
+    reed = loadImage('assets/reed.png');
     banner = loadImage('assets/banner.png');
     space = createVideo(['assets/space3.mp4'], vidLoad);
     // capture = createCapture(VIDEO);
@@ -41,6 +42,7 @@ function setup() {
 
     ufos = new UFOSystem();
     cats = new CatSystem();
+    reeds = new CatSystem();
 
     shootingStarSystem = new ParticleSystem();
     particleSystem = new ParticleSystem();
@@ -64,7 +66,7 @@ function draw() {
     // image(capture, width/2, height/2, 960, 720);
     image(space, width/2, height/2, width, height);
 
-    // Shooting stars on C4
+    // Shooting stars on C2
     if (notes[48] && gate0) {
         ufos.addUFO(createVector(0, height/2 * random()), createVector(10, 0));
         gate0 = 0;
@@ -73,16 +75,27 @@ function draw() {
         gate0 = 1;
     }
 
+    // Cats on C#2
     if (notes[49] && gate1) {
-        cats.addCat(createVector(width * random(), height * 2/3 * random()));
+        cats.addCat(createVector(width * random(), height * 2/3 * random()), cat);
         gate1 = 0;
     }
     if (!notes[49]) {
         gate1 = 1;
     }
 
+    // Reeds on D2
+    if (notes[50] && gate2) {
+        reeds.addCat(createVector(width * random(), height * 2/3 * random()), reed);
+        gate2 = 0;
+    }
+    if (!notes[50]) {
+        gate2 = 1;
+    }
+
     ufos.run();
     cats.run();
+    reeds.run();
 
     // Strobe D1 & Eb1
     if (notes[38]) {
@@ -96,10 +109,14 @@ function draw() {
     }
 
     if (notes[0]) {
-        image(ldlogo, 90, 90, 100, 100);
+        image(ldlogo, 80, 80, 100, 100);
         // image(banner, 15, 50, 60, 60);
     } else {
-        image(ldlogo, 90, 90, 120, 120);
+        image(ldlogo, 80, 80, 120, 120);
         // image(banner, 15, 50, 70, 70);
     }
+
+    // rectMode(CORNER);
+    // fill('red');
+    // rect(180, 40, 300, 80);
 }
